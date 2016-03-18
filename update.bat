b1d39982750f2282303ec5d578e2360e4f18f463
SyntaxPD <fromeast@rambler.ru>
2016-03-18 16:17:11 +0300

    Update bat script
    resets any  local changes

--- update.bat
+++ update.bat
@@ -1,2 +1,14 @@
-rd config /q /s
-git\git-cmd.exe git clone https://github.com/SyntaxPD/PowerDucks-minecraft-config.git config/
+echo off
+cls
+
+start /wait /b "" "git\git-cmd.exe" git remote add origin https://github.com/SyntaxPD/PowerDucks-minecraft-client.git
+timeout /t 3 /nobreak >nul
+
+start /wait /b "" "git\git-cmd.exe" git remote -v
+timeout /t 3 /nobreak >nul
+
+start /wait /b "" "git\git-cmd.exe" git pull
+timeout /t 3 /nobreak >nul
+
+start /wait /b "" "git\git-cmd.exe" git reset --hard origin/master
+timeout /t 3 /nobreak >nul
